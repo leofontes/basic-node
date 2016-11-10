0bf35457c831a66214a3e17b1242499068feb2bf
Leo Fontes <leofontes@gmail.com>
2016-11-10 20:16:39 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+"use strict";
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(PORT, function() {
+	console.log("Server started on port " + PORT);
+});
